cefc9e0a61a23de98de3bea7c21a832b9acc6bb9
satyam <satyamok@gmail.com>
2023-07-11 08:04:22 +0530

    header footer Material ui done

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Header.css';
+
+
+const Header = () => {
+  return (
+    <span className='header'> Movie Monkey 😆 </span>
+    
+  )
+}
+
+export default Header
---
+++ src/components/MainNav.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles = makeStyles({
+    root:{
+        width: 500,
+        position: "fixed",
+        bottom: 0,
+        background:"#39445a",
+        zIndex:100,
+    },
+});
+
+
+export default function SimpleBottomNavigation() {
+    const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+ 
+
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        className={classes.root} style={{ color: 'white', backgroundColor: '#39445a' }}
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
